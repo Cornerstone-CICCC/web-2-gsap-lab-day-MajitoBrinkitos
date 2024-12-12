c3f82a772459778c0e153abc74300dbe3897f72b
Majito Brinkitos <mcvef2013@gmail.com>
2024-12-12 10:33:16 -0800

    section1

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
 gsap.registerPlugin(ScrollTrigger);
 
 // YOUR CODE HERE
+//section 1 timeline
+const section1Tl = gsap.timeline({
+        trigger: '.section1',
+        start: 'top top',
+        end: '+=400',
+        scrub: 1,
+        yoyo: true,
+        markers: true,
+});
+
+//Hi
+section1Tl.from('.section1 h1 span:first-child', {
+    x: '-100vw',
+    opacity: 0,
+    duration: 2,
+    ease: 'power4.out'
+},
+"+=1");
+
+//I am John
+section1Tl.from('.section1 h1 span:nth-child(2)', {
+    x: '100vw',
+    opacity: 0,
+    duration: 2,
+    ease: 'power4.out',
+},
+"-=1");
+
+//rotating p
+section1Tl
+.from('.section1 p', {
+    x: '0vw',
+    transform: 'rotate(90deg) translateY(350%) translateX(40%)',
+    opacity: 0,
+    duration: 1,
+    ease: 'power4.out'
+})
+
+.to('.section1 p', {
+    transform: 'rotate(0deg) ',
+    opacity: 2,
+    duration: 1,
+    ease: 'power4.out'
+});
+
+//geek dog image
+section1Tl
+.from('.section1 img', {
+    y: '100vh',
+})
